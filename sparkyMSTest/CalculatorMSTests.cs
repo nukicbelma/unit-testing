f56a300f05ac60440b2f508325942ee812531c12
Belma Nukić <nukicbelma@hotmail.com>
2022-05-18 10:50:54 +0200

    Add project files.

---
+++ sparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sparkyMSTest
+{
+    class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //arrange
+            Calculator calc = new();
+
+            //act
+            int result = calc.AddNumbers(10, 20);
+
+            //assert
+            Assert.AreEqual(30, result);
+        }
+
+
+    }
+}
